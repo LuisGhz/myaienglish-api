d0ccfd349e0803241524cd1f5a3b84fa2660ac75
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-25 16:13:04 -0600

    Add translateText

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,13 +4,27 @@ import { Instruction } from './entities/instruction.entity';
 import { Repository } from 'typeorm';
 import { CreateInstructionDto } from './dtos/instructions/create-instruction.dto';
 import { UpdateInstructionDto } from './dtos/instructions/update-instruction.dto';
+import { TranslateTextReqDto } from './dtos/translate/translate-text.req.dto';
+import { OpenAIService } from './openai.service';
 
 @Injectable()
 export class AppService {
 
   constructor(
     @InjectRepository(Instruction) private instructionRepository: Repository<Instruction>,
+    private openAIService: OpenAIService
   ) { }
+
+  async translateText({ instructionId, context, textToTranslate }: TranslateTextReqDto) {
+    const instruction = await this.instructionRepository.findOneBy({ id: instructionId });
+    const prompt = `
+      You are an expert on languages translation your job is to help to the user with the given text ""${textToTranslate}"".
+      This according to the following instructions: ""${instruction!.content}""
+      ${context ? `Also consider the following context which can help you to give a better translation: ""${context}""` : ''}
+    `;
+
+    return this.openAIService.translateText(prompt);
+  }
 
   getInstructions() {
     return this.instructionRepository.find();
