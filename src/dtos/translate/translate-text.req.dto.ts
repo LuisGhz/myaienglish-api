3d40a97874a917aca7236f1f43f0dd5a7122b71a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-25 16:13:11 -0600

    Add translate req and res dtos

---
+++ src/dtos/translate/translate-text.req.dto.ts
@@ -0,0 +1,13 @@
+import { IsOptional, IsString } from "class-validator";
+
+export class TranslateTextReqDto {
+  @IsString()
+  instructionId: string;
+
+  @IsString()
+  @IsOptional()
+  context?: string;
+
+  @IsString()
+  textToTranslate: string;
+}
---
+++ src/dtos/translate/translate-text.res.dto.ts
@@ -0,0 +1,6 @@
+export interface TranslateTextResDto {
+  translatedText: string;
+  inputTokens?: number;
+  outputTokens?: number;
+  totalTokens?: number;
+}
