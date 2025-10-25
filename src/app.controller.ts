4f124712ebe20bbea60353d2298873632cd74ffd
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-25 13:51:26 -0600

    Add instructions post endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateInstructionDto } from './dtos/instructions/create-instruction.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('instructions')
+  async createInstruction(@Body() createInstructionDto: CreateInstructionDto) {
+    return this.appService.createInstruction(createInstructionDto);
   }
 }
