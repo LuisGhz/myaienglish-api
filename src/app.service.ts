df3be6993b7f5702ed1f13a46a3c2ef06d7fae1a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-25 13:52:38 -0600

    Add create instruction

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Instruction } from './entities/instruction.entity';
+import { Repository } from 'typeorm';
+import { CreateInstructionDto } from './dtos/instructions/create-instruction.dto';
 
 @Injectable()
 export class AppService {
+
+  constructor(
+    @InjectRepository(Instruction) private instructionRepository: Repository<Instruction>,
+  ) { }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createInstruction({ name, content }: CreateInstructionDto): Promise<Instruction> {
+    const instruction = this.instructionRepository.create({ name, content });
+    return this.instructionRepository.save(instruction);
   }
 }
