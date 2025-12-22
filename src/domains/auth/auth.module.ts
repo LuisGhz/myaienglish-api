54e6de659d87178a9652df995ca775cd91f2e983
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-22 13:24:14 -0600

    feat(auth): configure AuthModule with TypeORM and JWT setup

--- src/domains/auth/auth.module.ts
+++ src/domains/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { RefreshToken } from './entities';
+import { AuthService, RefreshTokenService } from './services';
+import { AuthController } from './controllers';
+import { UserModule } from '../user';
+import { ConfigModule, EnvService } from '../../config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-  exports: [],
+  imports: [
+    TypeOrmModule.forFeature([RefreshToken]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [EnvService],
+      useFactory: (envService: EnvService) => ({
+        secret: envService.jwtSecret,
+        signOptions: { expiresIn: envService.jwtExpiresIn as any },
+      }),
+    }),
+    UserModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, RefreshTokenService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/domains/auth/index.ts
+++ src/domains/auth/index.ts
@@ -1,1 +1,5 @@
 export * from './auth.module';
+export * from './entities';
+export * from './services';
+export * from './dtos';
+export * from './controllers';
