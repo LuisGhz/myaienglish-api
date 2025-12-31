f255069620daa43f8ba68bd8320543f16b9e5d46
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-12-31 15:39:33 -0600

    Refactor

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { EnvService } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const envService = app.get(EnvService);
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: envService.frontendUrl,
+    credentials: true,
+  });
+  await app.listen(envService.port);
 }
 bootstrap();
